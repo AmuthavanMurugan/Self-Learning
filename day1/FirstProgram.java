e92d8c11cbf33eb26ce991a39d1b04ca41d0a79a
AmuthavanMurugan <amuthavanmurugan25@gmail.com>
2026-06-10 23:31:57 +0530

    Add files via upload

---
+++ day1/FirstProgram.java
@@ -0,0 +1,31 @@
+package week.day1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Amuthavan");
+		System.out.println("---------------");
+		
+		int a = 10;
+		int b = 10;
+		int c = a + b;
+		System.out.println(c);
+		System.out.println("--------------");
+		
+		char Initial = 'M';
+		String Name = "Amuthavan";
+		System.out.println(Initial + "." +Name);
+		System.out.println("--------------");
+		
+		String BrowserName = "Chrome";
+		char Logo = 'C';
+		float BrowserVersion = 100.2f;
+		int ReleasedIn = 1997;
+		boolean IsVisible = true;
+		System.out.println("About Browser:" + " " + BrowserName + " " + BrowserVersion + " " + "Release Year" + " " + ReleasedIn + " " + Logo + " " + IsVisible);
+		System.out.println("--------------");
+
+	}
+
+}
